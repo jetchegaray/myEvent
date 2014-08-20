c879fb4e9537d3ead736b297d602ec755ad2e8d1
jetchegaray <jetchegaray@despegar.com>
2014-08-20 17:56:22 -0300

    -mvn spring init

--- mi-evento-service/src/main/java/com/je/enterprise/mievento/service/controller/HomeController.java
+++ mi-evento-service/src/main/java/com/je/enterprise/mievento/service/controller/HomeController.java
@@ -1,5 +1,18 @@
 package com.je.enterprise.mievento.service.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/Welcome")
 public class HomeController {
 
+	@RequestMapping(method = RequestMethod.GET)
+	public String welcome(ModelMap model){
+		model.addAttribute("message", "Mi evento up");
+		return "index";
+	}
 }
