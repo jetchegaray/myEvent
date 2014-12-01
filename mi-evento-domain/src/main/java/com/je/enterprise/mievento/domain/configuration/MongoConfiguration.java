ca2f23377f60134924190d556830481a85a04f0b
etchegarayjavier@gmail.com <etchegarayjavier@gmail.com>
2014-12-01 19:29:43 -0300

    -update mongo heroku -eilminate bower component

--- mi-evento-domain/src/main/java/com/je/enterprise/mievento/domain/configuration/MongoConfiguration.java
+++ mi-evento-domain/src/main/java/com/je/enterprise/mievento/domain/configuration/MongoConfiguration.java
@@ -21,32 +21,32 @@ public class MongoConfiguration {
 	public static final String DB_NAME = "mi-evento";
 	
 	
-	 @Bean
-    public DB getDb() throws UnknownHostException, MongoException {
-		 
-		 MongoClientURI uri  = new MongoClientURI("mongodb://javimetal:Dickis666@ds051740.mongolab.com:51740/heroku_app31605532"); 
-	     MongoClient client = new MongoClient(uri);
-	     DB db = client.getDB(uri.getDatabase());
-
-        return db;
-    }
+//	 @Bean
+//    public DB getDb() throws UnknownHostException, MongoException {
+//		 
+//		 MongoClientURI uri  = new MongoClientURI("mongodb://javimetal:Dickis666@ds051740.mongolab.com:51740/heroku_app31605532"); 
+//	     MongoClient client = new MongoClient(uri);
+//	     DB db = client.getDB(uri.getDatabase());
+//
+//        return db;
+//    }
 
 	@Bean
 	public Datastore MongoConnectionManager() {
 		MongoClient client = null;
-//		MongoClientURI uri  = new MongoClientURI("mongodb://javimetal:Dickis666@ds051740.mongolab.com:51740/heroku_app31605532"); 
+		MongoClientURI uri  = new MongoClientURI("mongodb://javimetal:Dickis666@ds051740.mongolab.com:51740/heroku_app31605532"); 
 			
 		try {
-//		     client = new MongoClient(uri);
-		   client = new MongoClient("localhost", 27017);
+		   client = new MongoClient(uri);
+//		   client = new MongoClient("localhost", 27017);
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
 		
 		Morphia morphia = new Morphia();
 		morphia.getMapper().getConverters().addConverter(BigDecimalConverter.class);
-		Datastore ds = morphia.createDatastore(client, DB_NAME);
-//		Datastore ds = morphia.createDatastore(client, uri.getDatabase());
+//		Datastore ds = morphia.createDatastore(client, DB_NAME);
+		Datastore ds = morphia.createDatastore(client, uri.getDatabase());
 		ds.setDefaultWriteConcern(WriteConcern.FSYNCED);
 		return ds;
 	}
