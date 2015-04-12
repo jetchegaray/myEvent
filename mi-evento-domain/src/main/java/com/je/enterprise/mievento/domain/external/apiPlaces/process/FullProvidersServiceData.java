ac79183a5fa4578cf29da8ede0751b12c537d431
etchegarayjavier@gmail.com <etchegarayjavier@gmail.com>
2015-04-12 17:19:41 -0300

    -change @cron

--- mi-evento-domain/src/main/java/com/je/enterprise/mievento/domain/external/apiPlaces/process/FullProvidersServiceData.java
+++ mi-evento-domain/src/main/java/com/je/enterprise/mievento/domain/external/apiPlaces/process/FullProvidersServiceData.java
@@ -54,7 +54,7 @@ public class FullProvidersServiceData {
 	
 	//couta 1k request/day
 //	@Scheduled(cron = "0 0  * * ?")
-	@Scheduled(cron = "0 13 21 * * ?")
+	@Scheduled(cron = "0 25 17 * * ?")
 	public void serviceProcessData() {
 		
 		List<DetailPlace> places = this.getData();
