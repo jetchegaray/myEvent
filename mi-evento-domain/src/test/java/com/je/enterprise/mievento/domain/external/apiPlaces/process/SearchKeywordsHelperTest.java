9eeb512328c9be45b5afc9c6b06d1d9e9f5ef67d
etchegarayjavier@gmail.com <etchegarayjavier@gmail.com>
2015-04-22 11:50:56 -0300

    -fix properties for keyword

--- mi-evento-domain/src/test/java/com/je/enterprise/mievento/domain/external/apiPlaces/process/SearchKeywordsHelperTest.java
+++ mi-evento-domain/src/test/java/com/je/enterprise/mievento/domain/external/apiPlaces/process/SearchKeywordsHelperTest.java
@@ -2,15 +2,23 @@ package com.je.enterprise.mievento.domain.external.apiPlaces.process;
 
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Set;
+import java.util.Map.Entry;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.google.common.collect.Sets;
+import com.je.enterprise.mievento.api.dto.provider.ProviderType;
 import com.je.enterprise.mievento.domain.context.ContextLocale;
+import com.je.enterprise.mievento.domain.external.apiPlaces.transformer.type.BuilderConditionRulesProvider;
+import com.je.enterprise.mievento.domain.external.apiPlaces.transformer.type.ConditionRuleProviderKeyWord;
 import com.je.enterprise.mievento.domain.utils.SearchKeywordsHelper;
 
 public class SearchKeywordsHelperTest {
@@ -27,19 +35,18 @@ public class SearchKeywordsHelperTest {
 	public void getKeywords_ok() throws IOException{
 		ContextLocale.getContextLocale().setLocale(new Locale("es"));
 		searchKeywordsHelper.load();
-
-		Set<String> filtering = searchKeywordsHelper.filtering(createListToFilter());
-		Iterator<String> iterator = filtering.iterator();
 		
-		Assert.assertEquals("", iterator.next());
-		Assert.assertEquals("", iterator.next());
-		Assert.assertEquals("", iterator.next());
-		Assert.assertEquals("", iterator.next());
-		Assert.assertEquals("", iterator.next());
+		 Map<ProviderType,List<ConditionRuleProviderKeyWord>> rulesByType = BuilderConditionRulesProvider.getRules();
+		 
+		 for (Entry<ProviderType, List<ConditionRuleProviderKeyWord>> entry : rulesByType.entrySet()) {
+			 List<ConditionRuleProviderKeyWord> rules = entry.getValue();
+			 
+			 for (ConditionRuleProviderKeyWord conditionRuleProviderKeyWord : rules) {
+				 Pair<String, String> args = conditionRuleProviderKeyWord.getArguments();
+				 Assert.assertNotEquals(args.getLeft(), StringUtils.EMPTY);
+				 Assert.assertNotEquals(args.getRight(), StringUtils.EMPTY);
+			}
+		 }
 	}
 	
-	private Set<String> createListToFilter(){
-		return Sets.newHashSet("keyword.catering","keyword.hall","keyword.marriage","keyword.wedding","keyword.bithday","keyword.child");
-	}
- 	
 }
